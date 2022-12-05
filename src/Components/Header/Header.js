e6be1b5d7eb228b49825effddee7002d0caae3ae
itsmaruf <maruf@corexlab.dev>
2022-12-05 11:55:15 +0600

    header done

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { FaArrowLeft } from "react-icons/fa";
+import { FaArrowLeft, FaCaretRight } from "react-icons/fa";
 
 const Header = () => {
   return (
@@ -14,9 +14,20 @@ const Header = () => {
             <a href="/" className="text-gray-600">
               Flows
             </a>
-            <span className="breadcrumb-separator">/</span>
-            <a href="/about">About</a>
+            <span className="breadcrumb-separator inline-block mx-2">/</span>
+            <a href="/about">Store Assistant</a>
           </div>
+        </div>
+
+        <div className="flex justify-end items-center">
+          <button className="btn btn-outline  text-white btn-sm border border-gray-400">
+            <FaCaretRight className="mr-2" />
+            Preview Demo
+          </button>
+          <button className="btn btn-primary  text-white btn-sm ml-3">
+            <FaCaretRight className="mr-2" />
+            Publish Now
+          </button>
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,14 @@ module.exports = {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["light"],
+    themes: {
+      mytheme: {
+        primary: "#a991f7",
+        secondary: "#f6d860",
+        accent: "#37cdbe",
+        neutral: "#3d4451",
+        "base-100": "#ffffff",
+      },
+    },
   },
 };
